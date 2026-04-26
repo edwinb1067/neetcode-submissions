11694906c2bc26949131be8db734870a1e15e3c1
Edwin Barrack <edwinb1067@vt.edu>
2026-04-26 09:47:10 -0400

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.py
@@ -0,0 +1,23 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minimum = []
+
+    def push(self, val: int) -> None:
+        if not self.stack or val <= self.minimum[-1]:
+            self.minimum.append(val)
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        if not self.stack:
+            return
+        if self.stack[-1] == self.minimum[-1]:
+            self.minimum.pop()
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minimum[-1]
